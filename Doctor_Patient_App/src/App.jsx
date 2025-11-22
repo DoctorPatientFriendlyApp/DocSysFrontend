50e21d4b87b40e8f3d38624029943a2e8be394b6
D1_89388_Rushikesh <rushikeshpatil23052002@gmail.com>
2025-11-23 00:20:48 +0530

    Refactor App.jsx: update routing structure and integrate new components

--- Doctor_Patient_App/src/App.jsx
+++ Doctor_Patient_App/src/App.jsx
@@ -1,62 +1,97 @@
-import React from "react";
-// ✅ Import Toastify
-import AddExistingPatients from "./pages/patients/AddExistingPatients";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
+  import React from "react";
+  // ✅ Import Toastify
+  import AddExistingPatients from "./pages/patients/AddExistingPatients";
+  import { ToastContainer } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import { useLocation } from "react-router-dom";
+  import { Routes, Route } from "react-router-dom";
+  import "./App.css";
 
-import PatientReports from "./pages/Patients/PatientReports";
-import PatientView from "./pages/Patients/PatientView";
-import PatientEdit from "./pages/Patients/PatientEdit";
-import DoctorPatients from "./pages/Doctors/DoctorPatients";
-import Login from "./pages/Login";
-import DoctorProfile from "./pages/Doctors/DoctorProfile";
-import DoctorList from "./components/Doctors/DoctorList";
-import AddDoctor from "./components/Doctors/AddDoctor";
-import PatientList from "./components/Patients/PatientList";
-import AddPatient from "./components/Patients/AddPatient";
-import Home from "./pages/Home";
-import EditDoctor from "./pages/Doctors/EditDoctor";
-import DoctorDashboard from "./pages/Doctors/DoctorDashboard";
-import PatientDashboard from "./pages/Patients/PatientDashboard";
-import NavBar from "./components/NavBar";
-import PatientDoctors from "./pages/Patients/PatientDoctors";
-import PatientTreatments from "./pages/Patients/PatientTreatments";
+  import ProtectedRoute from "./components/auth/ProtectedRoute";
+  import PatientReports from "./pages/Patients/PatientReports";
+  import PatientView from "./pages/Patients/PatientView";
+  import PatientEdit from "./pages/Patients/PatientEdit";
+  import DoctorPatients from "./pages/Doctors/DoctorPatients";
+  import Login from "./pages/Login";
+  import DoctorProfile from "./pages/Doctors/DoctorProfile";
+  import DoctorList from "./components/Doctors/DoctorList";
+  import AddDoctor from "./components/Doctors/AddDoctor";
+  import PatientList from "./components/Patients/PatientList";
+  import AddPatient from "./components/Patients/AddPatient";
+  import Home from "./pages/Home";
+  import EditDoctor from "./pages/Doctors/EditDoctor";
+  import DoctorDashboard from "./pages/Doctors/DoctorDashboard";
+  import PatientDashboard from "./pages/Patients/PatientDashboard";
+  import DoctorLayout from "./layouts/DoctorLayout";
+  import PatientLayout from "./layouts/PatientLayout";
+  import PublicLayout from "./layouts/PublicLayout";
+  import PatientDoctors from "./pages/Patients/PatientDoctors";
+  import PatientTreatments from "./pages/Patients/PatientTreatments";
+  import AccessDenied  from "./pages/AccessDenied";
 
-function App() {
-  return (
-    <>
-      {/* ✅ Navbar */}
-      <NavBar />
+  function App() {
 
-      {/* ✅ Routes */}
-      <Routes>
-         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<DoctorList />} />
-        <Route path="/doctors/add" element={<AddDoctor />} />
-        <Route path="/patients" element={<PatientList />} />
-        <Route path="/doctor/home" element={<DoctorDashboard />} />
-        <Route path="/patient/home" element={<PatientDashboard />} />
-        <Route path="/doctors/:id" element={<DoctorProfile />} />
-        <Route path="/doctors/:id/edit/" element={<EditDoctor />} />
-        <Route path="/patients/view/:id" element={<PatientView />} />
-        <Route path="/patients/edit/:id" element={<PatientEdit />} />
-        <Route path="/patients/:patientId/reports" element={<PatientReports />} />
-        <Route path="/patients/:id/treatments" element={<PatientTreatments />} />
-        <Route path="/doctor/patients" element={<DoctorPatients />} />
-       <Route path="/doctor/add-existing" element={<AddExistingPatients />} />
-       <Route path="/patients/:patientId/doctors" element={<PatientDoctors />} />
-                                   {/* optional doctorId */}
-        <Route path="/patients/add/:doctorId?" element={<AddPatient />} />
-      
-      </Routes>
+    const location = useLocation();
+    const user = JSON.parse(localStorage.getItem("user"));
+    const role = user?.role;
 
-        <ToastContainer position="top-center" autoClose={2000} />
-    </>
-  );
-}
+    const hideNavbarOn = ["/login"];
 
-export default App;
+    const showNavbar =
+      !hideNavbarOn.includes(location.pathname) &&
+      true;
+
+    return (
+      <>
+       <Routes>
+
+  {/* PUBLIC ROUTES */}
+  <Route element={<PublicLayout />}>
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/doctors/add" element={<AddDoctor />} />
+     <Route path="/patients/edit/:id" element={<PatientEdit />} />
+    <Route path="/access-denied" element={ <AccessDenied  /> } />
+
+  </Route>
+
+  {/* DOCTOR ROUTES */}
+  <Route element={<DoctorLayout />}>
+    <Route path="/doctor/home" element={<DoctorDashboard />} />
+    <Route path="/doctor/patients" element={<DoctorPatients />} />
+    <Route path="/doctor/add-existing" element={<AddExistingPatients />} />
+    <Route path="/doctors" element={<DoctorList />} />
+ 
+    <Route path="/doctors/:id" element={<DoctorProfile />} />
+    <Route path="/doctors/:id/edit" element={<EditDoctor />} />
+  </Route>
+
+  {/* PATIENT ROUTES */}
+  <Route element={<PatientLayout />}>
+    <Route path="/patient/home" element={
+       <ProtectedRoute allowedRoles={['PATIENT']}>
+           <PatientDashboard />
+        </ProtectedRoute> 
+        } />
+    <Route path="/patients" element={<PatientList />} />
+    <Route path="/patients/view/:id" element={<PatientView />} />
+    <Route path="/patients/:id/reports" element={<PatientReports />} />
+    <Route path="/patients/:id/treatments" element={<PatientTreatments />} />
+    <Route path="/patients/:patientId/doctors" element={ 
+      <ProtectedRoute allowedRoles={['PATIENT']}>
+          <PatientDoctors />
+      </ProtectedRoute> }   
+    />
+    <Route path="/patients/add/:doctorId?" element={<AddPatient />} />
+  </Route>
+  
+   
+</Routes>
+
+          <ToastContainer position="top-center" autoClose={2000} />
+      </>
+    );
+  }
+
+  export default App;
